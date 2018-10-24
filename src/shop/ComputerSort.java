4f9db377a9f7b41fb59ac1c94eac820701cf9c0f
Damian Juzyk <damian.juzyk@gmail.com>
2018-10-25 00:18:22 +0200

    add fix method

--- src/shop/ComputerSort.java
+++ src/shop/ComputerSort.java
@@ -8,7 +8,8 @@ public class ComputerSort {
         Computer[] computers = new Computer[]{
                 new Computer("Dell", 4500, 8),
                 new Computer("Apple", 3200, 16),
-                new Computer("Lenovo", 4100, 21)
+                new Computer("Lenovo", 4100, 32),
+                new Computer("Asus", 2400, 32)
         };
 
         boolean changes;
@@ -16,10 +17,13 @@ public class ComputerSort {
         do {
             changes = false;
             for (int i = 0; i < computers.length - 1; i++) {
-                if (computers[i].getCpuMhz() > computers[i + 1].getCpuMhz()) {
-                    Computer temp = computers[i];
-                    computers[i] = computers[i + 1];
-                    computers[i + 1] = temp;
+                Computer c1 = computers[i];
+                Computer c2 = computers[i + 1];
+
+                int compareResults = compareTo(c1, c2);
+
+                if (compareResults > 0) {
+                    swapElements(computers, i);
                     changes = true;
                 }
             }
@@ -27,6 +31,29 @@ public class ComputerSort {
 
         String arraysAsString = Arrays.toString(computers);
         System.out.println(arraysAsString);
+    }
+
+    private static void swapElements(Computer[] computers, int i) {
+        Computer temp = computers[i];
+        computers[i] = computers[i + 1];
+        computers[i + 1] = temp;
+    }
+
+    private static int compareTo(Computer c1, Computer c2) {
+        if (c1.getCpuMhz() > c2.getCpuMhz()) {
+            return 1;
+        } else if (c1.getCpuMhz() == c2.getCpuMhz()) {
+
+            if (c1.getRam() > c2.getRam()) {
+                return 1;
+            } else if (c1.getRam() == c2.getRam()) {
+                return c1.getName().compareTo(c2.getName());
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
+        }
     }
 
 
